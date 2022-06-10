9bcf35dd9169e9b44127875c02db7669b9d8cbed
soshirokov <soshirokov@gmail.com>
2022-06-10 17:59:21 +0300

    v2

--- src/Components/UserForm/index.tsx
+++ src/Components/UserForm/index.tsx
@@ -1,17 +1,9 @@
-import {
-  Button,
-  Cell,
-  Grid,
-  H,
-  Input,
-  InputDate,
-  Option,
-  Select,
-} from '@balance-pl/lego'
+import { Button, Cell, Grid, H, Input, Option, Select } from '@balance-pl/lego'
+import { Controller, useForm } from 'react-hook-form'
 
 const styles = {
   input: {
-    marginBottom: '15px',
+    marginTop: '15px',
   },
   h2: {
     marginTop: '25px',
@@ -19,37 +11,152 @@ const styles = {
 }
 
 export const UserForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      lastName: '',
+      firstName: '',
+      patronymic: '',
+      gender: '',
+      birthday: '',
+      phone: '',
+      email: '',
+      adress: '',
+      company: '',
+    },
+  })
+
+  const onSubmit = (data: any) => alert(JSON.stringify(data))
+
   return (
     <div className="UserForm">
       <H level="2" style={styles.h2}>
         Информация о сотруднике
       </H>
-      <form>
-        <Input style={styles.input} label="Фамилия" />
-        <Input style={styles.input} label="Имя" />
-        <Input style={styles.input} label="Отчество" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="lastName"
+          control={control}
+          rules={{ required: 'Поле является обязательным' }}
+          render={({ field }) => (
+            <Input
+              {...field}
+              style={styles.input}
+              label="Фамилия"
+              error={errors?.lastName && errors.lastName.message}
+            />
+          )}
+        />
+        <Controller
+          name="firstName"
+          control={control}
+          rules={{ required: 'Поле является обязательным' }}
+          render={({ field }) => (
+            <Input
+              {...field}
+              style={styles.input}
+              label="Имя"
+              error={errors?.firstName && errors.firstName.message}
+            />
+          )}
+        />
+        <Controller
+          name="patronymic"
+          control={control}
+          render={({ field }) => (
+            <Input {...field} style={styles.input} label="Отчество" />
+          )}
+        />
         <Grid cols={[1, 1]} gap={25}>
           <Cell>
-            <Select style={styles.input} label="Пол">
-              <Option value="0">Мужской</Option>
-              <Option value="1">Женский</Option>
-            </Select>
-            <Input
-              style={styles.input}
-              label="Мобильный телефон"
-              mask="+7 (999) 999-99-99"
+            <Controller
+              name="gender"
+              control={control}
+              render={({ field }) => (
+                <Select {...field} style={styles.input} label="Пол">
+                  <Option value="0">Мужской</Option>
+                  <Option value="1">Женский</Option>
+                </Select>
+              )}
+            />
+            <Controller
+              name="phone"
+              control={control}
+              rules={{ required: 'Поле является обязательным' }}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  style={styles.input}
+                  label="Мобильный телефон"
+                  error={errors?.phone && errors.phone.message}
+                  // mask="+7 (999) 999-99-99"
+                />
+              )}
             />
           </Cell>
           <Cell>
-            <InputDate label="Дата рождения" />
-            <Input style={styles.input} label="Email" />
+            <Controller
+              name="birthday"
+              control={control}
+              rules={{ required: 'Поле является обязательным' }}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  style={styles.input}
+                  label="Дата рождения"
+                  error={errors?.birthday && errors.birthday.message}
+                />
+              )}
+            />
+            <Controller
+              name="email"
+              control={control}
+              rules={{
+                required: 'Поле является обязательным',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Введен некорректный адрес почты',
+                },
+              }}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  style={styles.input}
+                  label="Email"
+                  error={errors?.email && errors.email.message}
+                />
+              )}
+            />
           </Cell>
         </Grid>
-        <Input style={styles.input} label="Адрес постоянной регистрации" />
-        <Input style={styles.input} label="Название работодателя" />
+        <Controller
+          name="adress"
+          control={control}
+          render={({ field }) => (
+            <Input
+              {...field}
+              style={styles.input}
+              label="Адрес постоянной регистрации"
+            />
+          )}
+        />
+        <Controller
+          name="company"
+          control={control}
+          render={({ field }) => (
+            <Input
+              {...field}
+              style={styles.input}
+              label="Название работодателя"
+            />
+          )}
+        />
         <Grid cols={[1, 1]} gap={25}>
           <Cell cols={[2, 2]}>
-            <Button size="m" block>
+            <Button style={styles.input} size="m" block>
               Сохранить
             </Button>
           </Cell>
